e40dca90399201b7c49de44e64a27e3858a0c136
Adam Soltys <asoltys@gmail.com>
2021-12-29 20:10:03 -0800

    address derivation example

---
+++ bip32.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"encoding/hex"
+	"fmt"
+
+	"github.com/btcsuite/btcd/btcec"
+	"github.com/tyler-smith/go-bip32"
+	"github.com/vulpemventures/go-elements/network"
+	"github.com/vulpemventures/go-elements/payment"
+)
+
+func main() {
+	bip32.PrivateWalletVersion, _ = hex.DecodeString("04358394")
+	bip32.PublicWalletVersion, _ = hex.DecodeString("043587cf")
+	root, _ := bip32.B58Deserialize("tprv8ZgxMBicQKsPdM1ejJrR8D3qr3h7wCPvDLcWmrBb5Miyrh34j3XzgghV2bvS1baNbLveYFCi87jpTdD7EUkxoJYaE6mS982ac9EdcBJoqVG")
+
+  // m/0'
+	x, _ := root.NewChildKey(bip32.FirstHardenedChild)
+
+  // m/0'/0'
+	y, _ := x.NewChildKey(bip32.FirstHardenedChild)
+
+  // m/0'/0'/91586'
+	z, _ := y.NewChildKey(bip32.FirstHardenedChild + 91587)
+
+	pubkey := z.PublicKey()
+
+	blindPrivkey, _ := btcec.NewPrivateKey(btcec.S256())
+	blindPubkey := blindPrivkey.PubKey()
+
+	pk, _ := btcec.ParsePubKey(pubkey.Key, btcec.S256())
+	p2wpkh := payment.FromPublicKey(pk, &network.Regtest, blindPubkey)
+	address, _ := p2wpkh.WitnessPubKeyHash()
+	fmt.Println(address)
+}
---
+++ go.mod
@@ -0,0 +1,17 @@
+module gokeys
+
+go 1.17
+
+require (
+	github.com/btcsuite/btcd v0.22.0-beta
+	github.com/tyler-smith/go-bip32 v1.0.0
+	github.com/vulpemventures/go-elements v0.3.6
+)
+
+require (
+	github.com/FactomProject/basen v0.0.0-20150613233007-fe3947df716e // indirect
+	github.com/FactomProject/btcutilecc v0.0.0-20130527213604-d3a63a5752ec // indirect
+	github.com/btcsuite/btclog v0.0.0-20170628155309-84c8d2346e9f // indirect
+	github.com/btcsuite/btcutil v1.0.3-0.20201208143702-a53e38424cce // indirect
+	golang.org/x/crypto v0.0.0-20201221181555-eec23a3978ad // indirect
+)
